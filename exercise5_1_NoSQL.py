ce737f033ee4e04b5f3dcfe5809876a0454a4243
faysoserious <32243939+faysoserious@users.noreply.github.com>
2017-10-25 20:58:06 +0200

    Update exercise5_1_NoSQL.py

--- exercise5_1_NoSQL.py
+++ exercise5_1_NoSQL.py
@@ -1,15 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 23 18:27:40 2017
-
-@author: fxie_
-"""
-
 from pymongo import MongoClient
+import pprint
+
+# Making a connection with MongoClient
 client = MongoClient('localhost',27017)
+#Getting a Database
 db = client.Northwind
-collection = db.customer
-result = collection.find_one()
-print(type(result))
-print(result)
-print(client.database_names())
+#Getting a collection
+#db.customers.aggregate returns command_cursor which can not print directly
+#use for ...in... to print
+with open('NOSQL_5_1.txt', 'wt') as f:
+    for orderid in db.orders.aggregate([
+    #specifies the collection in the same database to perform the join with
+    #        {"$lookup":{"from": "orders","localField": "CustomerID","foreignField": "CustomerID","as": "Customeritems"}},
+    
+            {"$lookup":{"from": "order-details","localField": "OrderID","foreignField": "OrderID","as": "orderdetails"}},
+                
+    #         {"$project":{"ProductID":1}},
+            {"$unwind":{ "path":"$orderdetails"}},
+            {"$lookup":{"from": "Products","localField": "orderdetails","foreignField": "ProductID","as": "productsdetails"}},
+            
+    #select the columns that we need
+           
+            {"$project":{"_id":0,"CustomerID":1,"OrderID":1,"orderdetails":{"ProductID":1,"productsdetails":{"ProductName":1}}}},
+    
+            {"$match" : {"CustomerID":"ALFKI"}}
+                            ]):
+    #Support to pretty-print lists, tuples, & dictionaries recursively.
+       print(orderid, file = f)
